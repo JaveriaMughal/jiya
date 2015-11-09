1e5075346d569121334256c7cb35e0cbd4aa926a
FA12-BSE-109 <FA12-BSE-109@LAB7C-09.cns.ciit>
2015-11-09 16:47:28 +0500

    C3

--- Myproject1/src/demo.java
+++ Myproject1/src/demo.java
@@ -4,6 +4,10 @@ public class demo {
 	public static void main(String[] args)
 	{
 		System.out.println("let do something using git");
+		System.out.println("hi hub");
+		System.out.println("my naM");
+		
+		
 		// TODO Auto-generated method stub
 
 	}
